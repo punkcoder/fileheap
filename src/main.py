595b5a7fb40e072fb2ddcdb73167b20e57c4d269
punkcoder <james.mckee.jr@gmail.com>
2023-11-29 19:45:38 +0000

    adding more of the html files for control.

--- src/main.py
+++ src/main.py
@@ -13,3 +13,16 @@ def index():
 @app.route('/folders?p=<query>')
 def folders(query):
     return render_template('folders.html', query=query)
+
+@app.route('/login')
+def loginpage():
+    return render_template('login.html')
+
+@app.route('/register')
+def registerpage():
+    return render_template('register.html')
+
+@app.route('/account/my')
+def myaccount():
+    return render_template('myaccount.html')
+    
